4a951b1a1005c10464e5f3243efec4c720c60b1a
Shreyasachan003 <136750882+Shreyasachan003@users.noreply.github.com>
2024-07-30 01:36:53 +0530

    portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,4 @@
-var typed = new Typed('.text', {
-    strings: ["Frontend Developer" , "Youtube", "Web Developer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
+var typed = new Typed('#element' , {
+    strings: ['<i>First</i> sentence.', 'amp; a second sentence.'],
+    typeSpeed: 50,
 });
